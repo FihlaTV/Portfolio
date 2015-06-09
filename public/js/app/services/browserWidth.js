8421fa1fb2854c9afc5e5e30ffe52051f2e3b7ab
manny <elnels@gmail.com>
2015-06-08 22:31:46 -0500

    Work section wrapped up

---
+++ public/js/app/services/browserWidth.js
@@ -0,0 +1,23 @@
+app.factory("browserWidth", function ($window) {
+	return {
+		/**
+		 * @Converts a JSON object to a querystring
+		 * @returns string
+		 */
+		 getBrowserWidth: function(){
+			return angular.element($window).width();
+		},
+		isDesktop:function(){
+			var width = this.getBrowserWidth();
+			return  width > 1024;
+		},
+		isTabletLarge:function(){
+			var width = this.getBrowserWidth(); 
+			return  width <= 1024 && width > 768;  
+		},
+		isTabletPortrait:function(){
+			var width = this.getBrowserWidth();
+			return  width <= 768 && width > 480;
+		}
+	};
+});
