46110b07e61c8708fcd2be9bf043ab5501e3fbf9
Emmanuel Nelson <mnelson@onenorth.com>
2017-01-11 23:49:18 -0600

    not running, but needed an update

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+//import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+// import { FormService } from './services/form.service';
+// import { FauxFormsService } from './services/in-memory.service';
+// import { RestService } from './services/rest.service';
+
+import {
+    AppComponent
+} from './components';
+
+@NgModule({
+    bootstrap: [ AppComponent ],
+    declarations: [ AppComponent ],
+    imports: [
+        BrowserModule,
+        HttpModule
+        // InMemoryWebApiModule.forRoot(FauxFormsService),
+        // ReactiveFormsModule
+    ],
+    //providers: [ FormService, RestService ]
+})
+export class AppModule {}
---
+++ src/components/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'dynamic-form-app',
+    template: require('./app.component.html')
+})
+export class AppComponent {
+    forms: FormData[] = null;
+    selectedForm: FormData = null;
+
+    // constructor(private formService: FormService, private restService: RestService) {
+    //     restService.getForms().subscribe((forms: FormData[]) => {
+    //         this.formService.setForms(forms);
+    //         this.forms = this.formService.getAllForms();
+    //     });
+    // }
+
+    // selectForm(formId: number) {
+    //     this.selectedForm = this.formService.getForm(formId);
+    // }
+}
